e854ee1e8171529e17a925a4d7db5a048861e339
mergimmdina-tech <mergimm.dina@gmail.com>
2025-10-18 14:07:05 +1400

    Add files via upload

---
+++ phppython.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector
+
+app = FastAPI()
+
+# Lejon kërkesa nga frontend lokal
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # mund ta limitosh vetëm për localhost
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Gimi2025",
+        database="databazaefundit"
+    )
+    return conn
+
+@app.post("/delete_all")
+async def delete_all():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM registration_details")
+                                # registration_details
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"success": True}
